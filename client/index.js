9e463080c99784ed3483d8b42ac70c51a7c77a65
emman31 <emman31@gmail.com>
2015-01-11 11:07:46 -0800

    Do not hardcode server name

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 $(window).load(function document_ready() {
-  socket = io.connect('http://162.248.162.83:8888/');
+  socket = io.connect(window.location.host);
 
   socket.emit('execute', 'NewGame', ['patate']);
 
